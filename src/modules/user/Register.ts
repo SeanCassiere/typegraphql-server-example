d2011ac3d36ca96696a86453b94b971fb4816074
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-09-15 11:13:03 +0530

    feat: make hello query publicly accessible

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,15 +1,13 @@
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import brcypt from "bcryptjs";
 
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
-import { isAuth } from "../middleware/isAuth";
 import { sendEmail } from "../utils/sendEmail";
 import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 
 @Resolver()
 export class RegisterResolver {
-	@UseMiddleware([isAuth])
 	@Query(() => String)
 	async hello() {
 		return "Hello World!";
